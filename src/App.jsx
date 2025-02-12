5d579e441ff25ce01ec51346b69f30503d256f8c
Jsr-17 <jarmoreno5@gmail.com>
2025-02-12 13:44:48 +0100

    Inicio de Proyecto de Pokemon

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import { Entrega } from "./Entrega";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <Entrega></Entrega>;
+}
+
+export default App;
