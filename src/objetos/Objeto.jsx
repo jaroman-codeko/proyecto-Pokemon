50a760c44b855a2f8e30fe574070dbe67a000e9c
jose antonio <joseCodeko@gmail.com>
2025-03-19 14:02:07 +0100

    feat:now the object have funcionality

--- src/objetos/Objeto.jsx
+++ src/objetos/Objeto.jsx
@@ -1,5 +1,32 @@
-import React from "react";
+import { useObjetos } from "../hooks/useObjetos";
+import "./Objeto.css";
 
-export const Objeto = () => {
-  return <div>Objeto</div>;
+export const Objetos = () => {
+  //custom hook encargado de recoger los datos de los objetos
+  const { cargando, objetos } = useObjetos();
+
+  return (
+    <>
+      {cargando ? (
+        "Cargando"
+      ) : (
+        <div className="objetos-container">
+          <h1>Objetos del Juego </h1>
+          {objetos.map(({ descripcion, categoria, image, nombre, efecto }) => (
+            <div key={nombre} className="objeto">
+              <div>
+                <h4>{nombre}</h4>
+                <p className="categoria">Categoría: {categoria}</p>
+                <p className="efecto">Efecto: {efecto}</p>
+              </div>
+              <div>
+                <img src={image} alt={nombre} />
+                <p className="descripcion">Descripción: {descripcion}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  );
 };
